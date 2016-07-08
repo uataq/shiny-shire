2091572187157915fe995061c09a92b4a7733c25
benfasoli <benfasoli@gmail.com>
2016-07-08 15:36:58 -0600

    Fixed missing loc_opts

--- auth/global.R
+++ auth/global.R
@@ -5,3 +5,7 @@ library(digest)
 library(shiny)
 library(shinydashboard)
 library(shinyBS)
+
+loc_opts <- c('asb', 'cpk', 'dbk', 'fru', 'heb', 'hpl', 'lgn', 'mur', 'nrd', 'roo', 'rpk', 'spl', 
+              'sug', 'sun', 'trx01', 'trx02', 'wbb', 'other')
+n2_sites <- c('dbk', 'heb', 'lgn', 'mur', 'rpk', 'sug', 'sun')
