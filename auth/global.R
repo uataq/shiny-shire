2f3c8243370e9464029ca2903093a82f7c9e2871
benfasoli <benfasoli@gmail.com>
2016-08-15 12:13:52 -0600

    Added specific asb tank room numbers

--- auth/global.R
+++ auth/global.R
@@ -7,6 +7,6 @@ library(shiny)
 library(shinydashboard)
 library(shinyBS)
 
-loc_opts <- c('asb', 'cpk', 'dbk', 'fru', 'hdp', 'heb', 'hpl', 'lgn', 'mur', 'nrd', 'roo', 'rpk', 'spl', 
+loc_opts <- c('asb279', 'asb286', 'cpk', 'dbk', 'fru', 'hdp', 'heb', 'hpl', 'lgn', 'mur', 'nrd', 'roo', 'rpk', 'spl', 
               'sug', 'sun', 'trx01', 'trx02', 'wbb', 'other')
 n2_sites <- c('dbk', 'heb', 'lgn', 'mur', 'rpk', 'sug', 'sun')
