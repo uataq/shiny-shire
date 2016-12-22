aa31082780782fe08eed4a80401f4bc3e7d513c6
benfasoli <benfasoli@gmail.com>
2016-12-22 11:40:12 -0700

    Changed imc notes

--- auth/global.R
+++ auth/global.R
@@ -7,6 +7,7 @@ library(shiny)
 library(shinydashboard)
 library(shinyBS)
 
-loc_opts <- c('asb279', 'asb286', 'cpk', 'dbk', 'fru', 'hdp', 'heb', 'hpl', 'lgn', 'mur', 'nrd', 'roo', 'rpk', 'spl', 
+loc_opts <- c('asb279', 'asb286', 'cpk', 'dbk', 'fru', 'hdp', 'heb', 'hpl',
+              'imc', 'lgn', 'nrd', 'roo', 'rpk', 'spl', 
               'sug', 'sun', 'trx01', 'trx02', 'wbb', 'other')
-n2_sites <- c('dbk', 'heb', 'lgn', 'mur', 'rpk', 'sug', 'sun')
+n2_sites <- c('dbk', 'heb', 'imc', 'lgn', 'rpk', 'sug', 'sun')
